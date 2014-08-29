4bcaa8d6a5d9c14324029c8f54f5eedd4a57fbf8
ikikko <ikikko+github@gmail.com>
2014-08-29 23:08:56 +0900

    Karabiner の設定ファイルを追加する

---
+++ karabiner-import.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.emacsmode_force_controlD_vm 1
+/bin/echo -n .
+$cli set option.emacsmode_controlK 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlH_eclipse 1
+/bin/echo -n .
+$cli set option.emacsmode_ex_controlG 1
+/bin/echo -n .
+$cli set option.emacsmode_controlH 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_optionD 1
+/bin/echo -n .
+$cli set option.emacsmode_controlY 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlD_eclipse 1
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlPNBF_vm 1
+/bin/echo -n .
+$cli set option.emacsmode_controlS 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF 1
+/bin/echo -n .
+$cli set option.emacsmode_optionLtGt 1
+/bin/echo -n .
+$cli set option.emacsmode_controlAE 1
+/bin/echo -n .
+$cli set private.emacsmode_force_controlAE_eclipse 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlD_x11 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlH_x11 1
+/bin/echo -n .
+$cli set option.emacsmode_OptionWCopy 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlH_vm 1
+/bin/echo -n .
+$cli set option.emacsmode_ex_controlX 1
+/bin/echo -n .
+$cli set option.emacsmode_controlD 1
+/bin/echo -n .
+$cli set remap.jis_unify_kana_to_eisuu 1
+/bin/echo -n .
+$cli set private.emacsmode_ex_controlSpace 1
+/bin/echo -n .
+$cli set private.emacsmode_force_controlV_eclipse 1
+/bin/echo -n .
+$cli set remap.jis_unify_eisuu_to_kana 1
+/bin/echo -n .
+$cli set general.suppress_growl_warning 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlPNBF_x11 1
+/bin/echo -n .
+$cli set option.emacsmode_force_controlPNBF_eclipse 1
+/bin/echo -n .
+$cli set private.emacsmode_force_commandV_eclipse 1
+/bin/echo -n .
+$cli set option.emacsmode_controlW 1
+/bin/echo -n .
+$cli set option.emacsmode_controlSlash 1
+/bin/echo -n .
+$cli set option.emacsmode_optionBF 1
+/bin/echo -n .
+/bin/echo
